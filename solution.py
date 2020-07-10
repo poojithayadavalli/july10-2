a6d08bc9236716ead7dd845d61825c3ebc98f315
PoojithaYadavalli <poojithareddyyadavalli@gmail.com>
2020-07-10 19:51:29 +0530

    Update solution.py

--- solution.py
+++ solution.py
@@ -2,3 +2,10 @@ class Node:
     def __init__(self,val):
         self.data=val
         self.next=None
+
+def display(head):
+    l=[]
+    while head:
+        l.append(str(head.data))
+        head=head.next
+    print("->".join(l))
