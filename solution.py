849199611a26bac1d8675b6c7b807724f69588e5
PoojithaYadavalli <poojithareddyyadavalli@gmail.com>
2020-07-10 20:02:58 +0530

    Update solution.py

--- solution.py
+++ solution.py
@@ -8,4 +8,23 @@ def display(head):
     while head:
         l.append(str(head.data))
         head=head.next
-    print("->".join(l))
+    print("  ".join(l))
+def linkedlist(arr):
+    head=head1=Node(arr[0])
+    for i in range(1,len(arr)):
+        head1.next=Node(arr[i])
+        head1=head1.next
+    return head
+def deleteNode(head,val):
+    temp=head
+    while temp and temp.next.data!=val:
+        temp=temp.next
+    if temp!=None:
+        temp.next=temp.next.next
+        
+arr=list(map(int,input()))
+val=int(input())
+head=linkedlist(arr)
+deleteNode(head,val)
+display(head)
+        
